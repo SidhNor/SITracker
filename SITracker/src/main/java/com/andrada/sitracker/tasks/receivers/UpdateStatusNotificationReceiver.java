ca625a39b78aea5047aeabff6e9fbcca40ebb2de
ggodonoga <gleb.godonoga@gmail.com>
2013-08-08 23:58:55 -0400

    Moved assets to art

--- SITracker/src/main/java/com/andrada/sitracker/tasks/receivers/UpdateStatusNotificationReceiver.java
+++ SITracker/src/main/java/com/andrada/sitracker/tasks/receivers/UpdateStatusNotificationReceiver.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Gleb Godonoga.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.andrada.sitracker.tasks.receivers;
 
 import android.app.Notification;
@@ -17,9 +33,6 @@ import com.andrada.sitracker.tasks.messages.UpdateSuccessfulIntentMessage;
 import com.google.analytics.tracking.android.EasyTracker;
 import com.google.analytics.tracking.android.Tracker;
 
-/**
- * Created by ggodonoga on 22/07/13.
- */
 
 public class UpdateStatusNotificationReceiver extends BroadcastReceiver {
 
