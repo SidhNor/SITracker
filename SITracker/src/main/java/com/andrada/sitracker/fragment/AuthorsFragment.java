6cce6b1bcf9ba8ec03735b36cb4e052fd5c4f66a
ggodonoga <gleb.godonoga@gmail.com>
2013-08-12 00:23:28 -0400

    Updated ormlite verison

--- SITracker/src/main/java/com/andrada/sitracker/fragment/AuthorsFragment.java
+++ SITracker/src/main/java/com/andrada/sitracker/fragment/AuthorsFragment.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Gleb Godonoga.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.andrada.sitracker.fragment;
 
 import android.content.Intent;
@@ -98,7 +114,6 @@ public class AuthorsFragment extends SherlockFragment implements AuthorUpdateSta
         // Set the item as checked to be highlighted
         adapter.setSelectedItem(currentAuthorIndex);
         adapter.notifyDataSetChanged();
-
     }
 
     @Override
--- SITracker/src/main/java/com/andrada/sitracker/fragment/adapters/AuthorsAdapter.java
+++ SITracker/src/main/java/com/andrada/sitracker/fragment/adapters/AuthorsAdapter.java
@@ -1,6 +1,23 @@
+/*
+ * Copyright 2013 Gleb Godonoga.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.andrada.sitracker.fragment.adapters;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.ViewGroup;
@@ -101,6 +118,17 @@ public class AuthorsAdapter extends BaseAdapter implements IsNewItemTappedListen
         }
         authorsItemView.bind(authors.get(position), position == mSelectedItem);
         return authorsItemView;
+    }
+
+    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+        AuthorItemView authorsItemView = AuthorItemView_.build(context);
+        authorsItemView.setListener(this);
+        return authorsItemView;
+    }
+
+    public void bindView(View itemView, Context context, Author item) {
+        AuthorItemView authorsItemView = (AuthorItemView) itemView;
+        authorsItemView.bind(item, mSelectedAuthorId == item.getId());
     }
 
     @Override
